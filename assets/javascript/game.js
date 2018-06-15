4899035f8b9ae1cc924b9bc1b2acbc586538adcd
djbatt <djbatt@live.com>
2018-06-15 11:51:14 -0400

    starting on the variables and button generation

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,10 +1,20 @@
-var queryUrl = "http://api.giphy.com/v1/gifs/search?q=" + currentTopic + "&api_key=BgLtfJu3WbjPzngLYydCY8wjCVQ8BLUx&rating=pg-13";
-var topics = ["birch", "oak", "sequoia", "sycamore", "spruce", "willow", "pine", "fig", "palm", "maple"];
-
-// loop through the topics and generate a <btn> for each topic
-// when the user clicks the topic, grab the queryUrl and run a search based on the currentTopic (the currentTopic is the one the user clicked)
-// append a new button if the user pushes a new topic using the text-input
-// for mine we will check for hover instead of on click to start the gif animation
-// remember we are not overwriting the gifs added, we need to append more gifs.
-
-// i will finish this over the next week, unfinished as of now
+$(document).ready(function(){
+    var queryUrl = "http://api.giphy.com/v1/gifs/search?q=" + topics + "&api_key=BgLtfJu3WbjPzngLYydCY8wjCVQ8BLUx&rating=pg-13";
+    var topics = ["birch", "oak", "sequoia", "sycamore", "spruce", "willow", "pine", "fig", "palm", "maple"];
+    
+    $(".btn").on("click", function(event){
+        var userInput = event;
+    });
+    
+    for (var i = 0; i < topics.length; i++) {
+        $("#topicColumn").append("<btn class='btn btn-primary'>test</btn>")
+    }
+    
+    // loop through the topics and generate a <btn> for each topic
+    // when the user clicks the topic, grab the queryUrl and run a search based on the currentTopic (the currentTopic is the one the user clicked)
+    // append a new button if the user pushes a new topic using the text-input
+    // for mine we will check for hover instead of on click to start the gif animation
+    // remember we are not overwriting the gifs added, we need to append more gifs.
+    
+    // i will finish this over the next week, unfinished as of now
+});
